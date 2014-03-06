08715305cb4213f9f204f8e773518c7aeb411ce1
Aitor21 <aitormorales94@gmail.com>
2014-03-06 08:35:15 -0500

    added gravity

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,9 @@
 
   function Game() {
     this.player = null;
+    this.bg;
+    this.map;
+    this.layer;
   }
 
   Game.prototype = {
@@ -14,36 +17,34 @@
       this.player = this.add.sprite(x - 400, y - 900, 'player');
       this.player.anchor.setTo(0.5, 0.5);
       this.input.onDown.add(this.onInputDown, this);
+
+/////////////////////
+      if (this.input.keyboard.isDown(Phaser.Keyboard.A) && this.player.x>10)
+      {
+      this.player.x -=9,2;
+      }
+      else if (this.input.keyboard.isDown(Phaser.Keyboard.D) &&this.player.x<1000)
+      {
+      this.player.x +=9,2;       
+      }
+      this.player.y >10;
+      this.player.y <200;
+
+    this.stage.backgroundColor = '#000000';
+
+      this.physics.gravity.y = 10;
+      this.physics.setBoundsToWorld();
+      this.player.body.bounce.y = 0.2;
+      //this.player.body.minVelocity.y += 5;
+      this.player.body.collideWorldBounds = true;
+      this.player.body.setRectangle(16, 32, 8, 16);
+
     },
 
     update: function () {
       var x, y, cx, cy, dx, dy, angle, scale;
       var LEFTKey;
       var RIGHTKey;
-
-      if (this.input.keyboard.isDown(Phaser.Keyboard.A) && this.player.x>10)
-      {
-      this.player.x -=9,2;
-      }
-      else if (this.input.keyboard.isDown(Phaser.Keyboard.D) &&this.player.x<1018)
-      {
-      this.player.x +=9,2;
-      }
-
-      x = this.input.position.x;
-      y = this.input.position.y;
-      cx = this.world.centerX;
-      cy = this.world.centerY;
-
-      angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
-      this.player.angle = angle;
-
-      dx = x - cx;
-      dy = y - cy;
-      scale = Math.sqrt(dx * dx + dy * dy) / 100;
-
-      this.player.scale.x = scale * 0.6;
-      this.player.scale.y = scale * 0.6;
     },
 
     onInputDown: function () {
